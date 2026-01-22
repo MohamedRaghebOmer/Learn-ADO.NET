ec6a1450c6bf4c55a86d78d525ed27a71f8ea354
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-01-22 11:50:55 +0200

    finish practice on 'Column properties'

---
+++ DataTable/ColumnProperties/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //DataTable dtEmployees = new DataTable("Employees");
+
+        // dtEmployees.Columns.Add("ID", typeof(int));
+        // dtEmployees.Columns.Add("Name", typeof(string));
+        // dtEmployees.Columns.Add("Country", typeof(string));
+        // dtEmployees.Columns.Add("Salary", typeof(double));
+        // dtEmployees.Columns.Add("DateOfBirth", typeof(DateTime));
+
+        // DataColumn[] PKID = new DataColumn[1];
+        // PKID[0] = dtEmployees.Columns["ID"];
+        // dtEmployees.PrimaryKey = PKID;
+
+        DataColumn ID = new DataColumn();
+        ID.ColumnName = "ID";
+        ID.Caption = "Employees ID";
+        ID.ReadOnly = true;
+        ID.AutoIncrement = true;
+        ID.AutoIncrementSeed = 1;
+        ID.AutoIncrementStep = 1;
+
+
+        // Name properties
+        DataColumn Name = new DataColumn();
+        Name.ColumnName = "Name";
+        Name.Caption = "Employees names";
+        Name.MaxLength = 30;
+        Name.Unique = false;
+        Name.AutoIncrement = false;
+        Name.ReadOnly = false;
+        
+    }
+}
