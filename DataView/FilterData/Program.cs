0664ad4972af2032896030592a9216c5dfe2f109
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-01-23 04:51:38 +0200

    Finish practice on 'Create a filter on DataView'

---
+++ DataView/FilterData/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataTable employees = new DataTable("Employees");
+
+        employees.Columns.Add("Id", typeof(int));
+        employees.Columns.Add("Name", typeof(string));
+        employees.Columns.Add("Country", typeof(string));
+        employees.Columns.Add("Salary", typeof(decimal));
+        employees.Columns.Add("HireDate", typeof(DateTime));
+
+        employees.Rows.Add(1, "Mohamed", "Egypt", 10000m, new DateTime(2008, 1, 1));
+        employees.Rows.Add(2, "Ibrahem", "Jordan", 50030m, new DateTime(2006, 4, 21));
+        employees.Rows.Add(3, "Mostafa", "Kuwait", 43232m, new DateTime(2009, 7, 15));
+
+
+        DataView dvEmployees = employees.DefaultView;
+
+        // Create DataView filtering
+        dvEmployees.RowFilter = "Country = 'Egypt' OR Country = 'Jordan'";
+
+        Console.WriteLine("Printing data from DataView after filtering by country = Egypt or country = Jordan\n");
+        for (int i = 0; i < dvEmployees.Count; i++)
+        {
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}", dvEmployees[i][0], dvEmployees[i][1],
+                dvEmployees[i][2], dvEmployees[i][3], ((DateTime)dvEmployees[i][4]).ToString("dd/MM/yyyy"));
+        }
+    }
+}
+
